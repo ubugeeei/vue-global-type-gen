d93f07fa53c4c67db906ebb5e9c8f191b3e6760b
Ubugeeei <ubuge1122@gamil.com>
2022-11-10 23:26:37 +0900

    feat: genarate

--- src/generate.ts
+++ src/generate.ts
@@ -1,8 +1,32 @@
-export const generate = (argv: {
+import fs from 'fs'
+
+export const generate = ({
+  out,
+  dir,
+  include,
+  exclude
+}: {
   out: string
   dir: string
   include: string[]
   exclude: string[]
 }) => {
-  console.log(argv)
+  const listFiles = (dir: string): string[] =>
+    fs
+      .readdirSync(dir, { withFileTypes: true })
+      .flatMap(dirent =>
+        dirent.isFile() ? [`${dir}/${dirent.name}`] : listFiles(`${dir}/${dirent.name}`)
+      )
+
+  const types = listFiles(dir)
+    .map(
+      it =>
+        `\x20\x20export type ${it
+          .split('/')
+          .at(-1)
+          ?.replace(/\.vue/, '')} = typeof import('${it}').default;\n`
+    )
+    .join('')
+
+  fs.writeFileSync(out, `declare module '@vue/runtime-core' {\n${types}\n}`)
 }
