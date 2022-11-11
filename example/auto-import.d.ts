0cbc22690b0525d52ac1b07817a12f4c7cd80ff3
Ubugeeei <ubuge1122@gamil.com>
2022-11-11 11:49:23 +0900

    feat: example

---
+++ example/auto-import.d.ts
@@ -0,0 +1,14 @@
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    ErrorDialog: typeof import('example/components/dialogs/ErrorDialog.vue').default;
+    ConfirmationDialog: typeof import('example/components/dialogs/ConfirmationDialog.vue').default;
+    TextBtn: typeof import('example/components/TextBtn.vue').default;
+    Tag: typeof import('example/components/Tag.vue').default;
+    Logo: typeof import('example/components/Logo.vue').default;
+    LazyLogo: typeof import('example/components/Logo.vue').default;
+    LazyTag: typeof import('example/components/Tag.vue').default;
+    LazyTextBtn: typeof import('example/components/TextBtn.vue').default;
+    LazyConfirmationDialog: typeof import('example/components/dialogs/ConfirmationDialog.vue').default;
+    LazyErrorDialog: typeof import('example/components/dialogs/ErrorDialog.vue').default;
+  }
+}
